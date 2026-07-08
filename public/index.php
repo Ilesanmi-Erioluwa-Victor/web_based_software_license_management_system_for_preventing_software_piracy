27ab23efb872b463de0428961f40dd19a9f8b106
Ilesanmi Erioluwa Victor <ilesanmierioluwavictor@gmail.com>
2026-07-08 05:47:22 +0100

    Fix: serve SPA for non-API routes, route API through bootstrap

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if (!str_starts_with($uri, '/api/')) {
+    require __DIR__ . '/index.html';
+    exit;
+}
+
 require_once __DIR__ . '/../src/bootstrap.php';
